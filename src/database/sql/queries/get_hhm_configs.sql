76479cf2680fc20ad8694938875ec92394e8a7d2
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2025-07-28 18:23:14 +0000

    Removed LIKE clause for =

--- src/database/sql/queries/get_hhm_configs.sql
+++ src/database/sql/queries/get_hhm_configs.sql
@@ -18,10 +18,9 @@ FROM
     JOIN hhm_credentials_rust crd ON crd.id = ac.credentials_group::int
 WHERE
     sys.manufacturer = $1
-    AND sys.modality LIKE $2
+    AND sys.modality = $2
     AND sys.process_log = true;
 
     -- GE_CT ('SME17378', 'SME17368', 'SME17377');
     -- GE CV ('SME02412', 'SME00865');
    -- GE MRI ('SME02582', 'SME02583', 'SME19647', 'SME17380'); offline ('SME17382')
-   --
